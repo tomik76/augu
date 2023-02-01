71a6647967db17ea1e7f29a98139d332e23e57a4
tomik76 <98682579+tomik76@users.noreply.github.com>
2023-02-01 19:33:11 +0100

    Update main.java

--- main.java
+++ main.java
@@ -15,7 +15,7 @@ import com.google.ar.sceneform.ux.TransformableNode;
 public class MainActivity extends AppCompatActivity {
 
   private ArFragment arFragment;
-
+//ddddddd
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
